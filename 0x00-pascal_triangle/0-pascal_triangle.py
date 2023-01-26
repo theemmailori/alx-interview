eb1d4475b55100beb2933c3a611bb21ecae55e28
“victornnamdii” <victorilodiuba@gmail.com>
2023-01-26 23:25:28 +0100

    🧼 Clean up code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,21 +11,18 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    result_list = []
-    result_list.append([1])
+    triangle = [[1]]
 
     for row in range(2, n + 1):
-        row_list = []
+        rows = []
+
         for idx in range(row):
-            if idx == 0:
-                row_list.append(1)
+            if idx == 0 or idx == row - 1:
+                rows.append(1)
                 continue
-            if idx == row - 1:
-                row_list.append(1)
-                break
-            element = result_list[row - 2][idx] + result_list[row - 2][idx - 1]
-            row_list.append(element)
+            value = triangle[row - 2][idx] + triangle[row - 2][idx - 1]
+            rows.append(value)
 
-        result_list.append(row_list)
+        triangle.append(rows)
 
-    return result_list
+    return triangle
