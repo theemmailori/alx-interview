2675132530e13365af4ea111cda7977e76ddb5fa
“victornnamdii” <victorilodiuba@gmail.com>
2023-01-31 21:26:12 +0100

    Fix code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,13 +8,14 @@ def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened
     """
-    keys = []
-    for box in boxes:
-        try:
-            keys.append(box[0])
-        except IndexError:
-            pass
-    for required_key in range(1, len(boxes)):
-        if required_key not in keys:
-            return False
-    return True
+    keys = set()
+
+    for box_id, box in enumerate(boxes):
+        if len(box) == 0 or box_id == 0:
+            keys.add(box_id)
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                keys.add(key)
+        if len(keys) == len(boxes):
+            return True
+    return False
