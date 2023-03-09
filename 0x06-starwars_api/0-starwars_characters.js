e60767a0460c2e19312ff1e186d004faaae6ac4d
“victornnamdii” <victorilodiuba@gmail.com>
2023-03-09 14:33:17 +0100

    Add Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+/**
+ * Script for printing out all Star Wars Characters
+ */
+
+const request = require('request');
+const id = process.argv[2];
+if (!id || isNaN(id)) {
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const responses = [];
+
+  const json = JSON.parse(body);
+  const characters = json.characters;
+
+  characters.forEach((character) => {
+    const url = character;
+    const promise = new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(JSON.parse(body).name);
+      });
+    });
+    responses.push(promise);
+  });
+  Promise.all(responses)
+    .then(names => console.log(names.join('\n')))
+    .catch(errors => console.log(errors));
+});
